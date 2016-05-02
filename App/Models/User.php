05ced77c35b7e841d3b48f33c0a3b0311f96e7d8
Mironchikov Ilya <mir19on@mail.ru>
2016-05-02 22:11:56 +0300

    + ::findAll()

--- App/Models/User.php
+++ App/Models/User.php
@@ -2,15 +2,23 @@
 
 namespace App\Models;
 
+use App\Db;
 use App\Model;
 
-class User extends Model
-{
+class User extends Model{
     const TABLE = 'users';
 
     public $email;
     public $name;
     public $age;
     public $phone;
+
+    /*
+     * данная функция сделана статической, чтобы не создавался объект пользователя, когда нужно найти всех пользователей
+     */
+    public static function findAll(){
+        $db = new Db();
+        return $db->query("SELECT * FROM users", 'App\Models\User');
+    }
 
 }
