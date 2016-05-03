110a2c8e7f21973222d5baf2223020eaf25d8eaf
Mironchikov Ilya <mir19on@mail.ru>
2016-05-04 00:31:07 +0300

    public function insert()

--- App/Model.php
+++ App/Model.php
@@ -5,7 +5,10 @@ namespace App;
 abstract class Model
 {
 
+
     const TABLE = '';
+
+    public $id;
 
     public static function findAll()
     {
@@ -19,6 +22,31 @@ abstract class Model
     public static function findById($id){
         $db = Db::instance();
         return $db->query('SELECT * FROM ' . static::TABLE . " WHERE id = '{$id}'", static::class);
+    }
+
+
+    public function isNew(){
+        return empty($this->id);
+    }
+
+    public function insert(){
+        if(!$this->isNew()){
+            return;
+        }
+        $columns = [];
+        $values = [];
+        foreach($this as $k => $v){
+            if('id' == $k){
+                continue;
+            }
+            $columns[] = $k;
+            $values[':' .$k] = $v;
+        }
+        //var_dump($values);
+
+        $sql = 'INSERT INTO ' . static::TABLE . '('. implode(',', $columns) . ') VALUES('. implode(',', array_keys($values)).')';
+        $db = Db::instance();
+        $db->execute($sql, $values);
     }
 
 }
