b447b47d129df48de912a28b886d5311346874c2
Mironchikov Ilya <mir19on@mail.ru>
2016-05-03 20:09:39 +0300

    added foreach for show news

--- App/templates/index.php
+++ App/templates/index.php
@@ -1,7 +1,8 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 03.05.2016
- * Time: 19:50
- */
+<?php foreach ($news as $new):?>
+    <div class="new">
+        <h2 class="title"><?=$new->title?></h2>
+        <p class="cintent"><?=$new->content?></p>
+        <em><?=$new->author?></em>
+        <p><a href="article.php?id=<?=1?>">Подробнее</a></p>
+    </div>
+<?php endforeach;?>
