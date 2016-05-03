115eae4a70abee20eb6361d7659b9b331b6f7f4c
Mironchikov Ilya <mir19on@mail.ru>
2016-05-03 20:08:22 +0300

    added function findLastNews()

--- App/Models/News.php
+++ App/Models/News.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use App\Model;
+use App\Db;
 
 class News extends Model{
 
     const TABLE = 'news';
 
+    public static function findLastNews(){
+        $db = new Db();
+        return $db->query("SELECT * FROM " . self::TABLE . " ORDER BY id DESC LIMIT 3", self::class);
+    }
 
 }
