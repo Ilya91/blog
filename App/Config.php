b13fd2ebb2cf0731fb4d6c1ecd78a5b46487bae4
Mironchikov Ilya <mir19on@mail.ru>
2016-05-04 11:20:58 +0300

    создан класс Config

---
+++ App/Config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 03.05.2016
+ * Time: 22:12
+ */
+
+namespace App;
+
+
+class Config
+{
+
+    use Singleton;
+
+    protected function __construct()
+    {
+
+    }
+}
