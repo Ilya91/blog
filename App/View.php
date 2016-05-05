e2f97c4c4d9ad48623fa9f48b5cfd172d8cb67ac
Mironchikov Ilya <mir19on@mail.ru>
2016-05-05 21:27:07 +0300

    get(), display()

--- App/View.php
+++ App/View.php
@@ -5,8 +5,20 @@ namespace App;
 
 class View
 {
+
+    protected $data = [];
+
     public function __set($name, $value)
     {
-        // TODO: Implement __set() method.
+        $this->data[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->data[$name];
+    }
+
+    public function display($template){
+        include $template;
     }
 }
