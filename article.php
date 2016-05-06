588a3659bb3ec4fda33873d26b8da960b732b4f2
Mironchikov Ilya <mir19on@mail.ru>
2016-05-06 09:20:53 +0300

    $view = new \App\View();

--- article.php
+++ article.php
@@ -3,14 +3,9 @@ include __DIR__ . '/App/templates/header.php';
 require __DIR__ . '/autoload.php';
 if(isset($_GET['id'])){
     $id = $_GET['id'];
-    $news = \App\Models\News::findById($id);
+    $view = new \App\View();
+    $view->news = \App\Models\News::findById($id);
 }
 ?>
-<?php foreach ($news as $new):?>
-<div class="new">
-    <h2 class="title"><?=$new->title?></h2>
-    <p class="cintent"><?=$new->content?></p>
-    <em><?=$new->author?></em>
-</div>
-<?php endforeach;?>
+<?php echo $view->render(__DIR__ . '/App/templates/article.php');?>
 <?php include __DIR__ . '/App/templates/footer.php';?>
