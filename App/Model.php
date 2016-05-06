b82a787bb965f9a22891ba1be248906a68463937
Mironchikov Ilya <mir19on@mail.ru>
2016-05-06 22:41:55 +0300

    findById($id)   [0] - вывод чистого объекта, не в массиве

--- App/Model.php
+++ App/Model.php
@@ -21,7 +21,7 @@ abstract class Model
 
     public static function findById($id){
         $db = Db::instance();
-        return $db->query('SELECT * FROM ' . static::TABLE . " WHERE id = '{$id}'", static::class);
+        return $db->query('SELECT * FROM ' . static::TABLE . " WHERE id = '{$id}'", static::class)[0];
     }
 
 
