9e68f0d7c539942647587fc9945e93854586a0c6
Mironchikov Ilya <mir19on@mail.ru>
2016-05-06 22:25:19 +0300

    functions

--- App/Controllers/News.php
+++ App/Controllers/News.php
@@ -7,9 +7,50 @@
  */
 
 namespace App\Controllers;
-
+use App\View;
 
 class News
 {
 
+    protected $view;
+
+    /**
+     * News constructor.
+     */
+    public function __construct()
+    {
+        $this->view = new View();
+    }
+
+    public function action($action)
+    {
+        $methodName = 'action' . $action;
+        $this->beforeAction();
+        return $this->$methodName();
+    }
+
+
+    protected function beforeAction()
+    {
+         echo 'Counter';
+    }
+
+
+    protected function actionIndex()
+    {
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $item = new \App\Models\News();
+            $item->title = $_POST['title'];
+            //$item->author = $_POST['author'];
+            $item->content = $_POST['content'];
+            $item->save();
+            header('Location: index.php');
+        }
+
+        $this->view->display(__DIR__ . '/../templates/header.php');
+        $this->view->news = \App\Models\News::findLastNews();
+        $this->view->display(__DIR__ . '/../templates/index.php');
+        $this->view->display(__DIR__ . '/../templates/footer.php');
+    }
 }
