a4805b5a03ce52471902f3c4c98aad62b0c9dc0e
Mironchikov Ilya <mir19on@mail.ru>
2016-05-06 22:38:46 +0300

    оптимизирована старница

--- article.php
+++ article.php
@@ -1,11 +1,5 @@
 <?php
-include __DIR__ . '/App/templates/header.php';
 require __DIR__ . '/autoload.php';
-if(isset($_GET['id'])){
-    $id = $_GET['id'];
-    $view = new \App\View();
-    $view->news = \App\Models\News::findById($id);
-}
-?>
-<?php echo $view->render(__DIR__ . '/App/templates/article.php');?>
-<?php include __DIR__ . '/App/templates/footer.php';?>
+
+$controller = new \App\Controllers\News();
+$controller->action('One');
