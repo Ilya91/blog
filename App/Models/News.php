e713089413d7b047db319fd49c78ce95f24bd794
Mironchikov Ilya <mir19on@mail.ru>
2016-05-06 12:06:35 +0300

    Lazy Load

--- App/Models/News.php
+++ App/Models/News.php
@@ -4,14 +4,51 @@ namespace App\Models;
 
 use App\Model;
 use App\Db;
+use App\View;
+
+/**
+ * Class News
+ * @package App\Models
+ *
+ * @property \App\Models\Author $author
+ */
+
 
 class News extends Model{
 
+    use View;
     const TABLE = 'news';
 
+
+    public $title;
+    public $content;
+    public $author_id;
+
+    /**
+     * Lazy Load - ленивая загрузка(делаем запрос к базе на получение связанных данных, только тогда, когда они реально нужны)
+     * @param $name
+     * @return null
+     */
+    public function __get($name)
+    {
+        switch($name){
+            case 'author':
+                return Author::findById($this->author_id);
+                break;
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * @return mixed
+     * Запрашивает 3 последние новости
+     */
     public static function findLastNews(){
         $db = Db::instance();
         return $db->query("SELECT * FROM " . self::TABLE . " ORDER BY id DESC LIMIT 3", self::class);
     }
+
+
 
 }
