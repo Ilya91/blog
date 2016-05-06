4dcecf78e5554c65e914d63c625ddcfca84a0020
Mironchikov Ilya <mir19on@mail.ru>
2016-05-06 22:38:19 +0300

    оптимизирована страница

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
 require __DIR__ . '/autoload.php';
-$news = \App\Models\News::findLastNews();
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $item = new \App\Models\News();
-    $item->title = $_POST['title'];
-    $item->author = $_POST['author'];
-    $item->content = $_POST['content'];
-    $item->save();
-    header('Location: index.php');
-}
-include __DIR__ . '/App/templates/header.php';
-include __DIR__ . '/App/templates/index.php';
-include __DIR__ . '/App/templates/footer.php';
+
+$controller = new \App\Controllers\News();
+$controller->action('Index');
+
+
