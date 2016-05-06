b15a415933d564c448a3b8af981011d87b25db15
Mironchikov Ilya <mir19on@mail.ru>
2016-05-06 22:40:48 +0300

    $news->...

--- App/templates/article.php
+++ App/templates/article.php
@@ -1,7 +1,5 @@
-<?php foreach($this->news as $item):?>
 <div class="new">
-        <h2 class="title"><?=$item->title?></h2>
-        <p class="content"><?=$item->content?></p>
-        <em><?=$item->author?></em>
+        <h2 class="title"><?=$news->title?></h2>
+        <p class="content"><?=$news->content?></p>
+        <em><?=$news->author->name?></em>
     </div>
-<?php endforeach;?>
