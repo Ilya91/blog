05887be99714587eb2112c3c77bb95e5b0fe9764
Mironchikov Ilya <mir19on@mail.ru>
2016-05-09 22:29:25 +0300

    view - index

--- App/templates/index.php
+++ App/templates/index.php
@@ -1,8 +1,8 @@
 <?php foreach ($news as $new):?>
     <div class="new">
         <h2 class="title"><?=$new->title?></h2>
-        <p class="cintent"><?=$new->content?></p>
+        <p class="content"><?=$new->content?></p>
         <em><?=$new->author?></em>
-        <p><a href="article.php?id=<?=$new->id?>">Подробнее</a></p>
+        <p><a href="/news/article/<?=$new->id?>">Подробнее</a></p>
     </div>
 <?php endforeach;?>
