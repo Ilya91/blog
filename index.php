49a7db2a50b8cd7b4b381a04c105cb4ba9bf7f14
Mironchikov Ilya <mir19on@mail.ru>
2016-05-09 23:05:47 +0300

    optimized frontcontroller

--- index.php
+++ index.php
@@ -1,39 +1,9 @@
 <?php
 require __DIR__ . '/autoload.php';
 
-$url = $_SERVER['REQUEST_URI'];
-
-$info = explode('/', $url);
-$params = array();
-
-
-foreach ($info as $v)
-{
-    if ($v != '')
-        $params[] = $v;
-}
-
-$action = 'action_';
-$action .= (isset($params[1]) ? $params[1] : 'index');
-
-switch($params[0]){
-    case 'news':
-        $controller = new \App\Controllers\News();
-        break;
-    case 'admin':
-        $controller = new \App\Controllers\Admin();
-        break;
-    case null:
-        $controller = new \App\Controllers\News();
-        $action = 'action_index';
-        break;
-    default:
-        $controller = new \App\Controllers\News();
-        $action = 'action_index';
-        break;
-}
-//var_dump($action);
-//var_dump($_SERVER['REQUEST_URI']);
+$rout = new \App\Rout($_SERVER['REQUEST_URI']);
+$controller = $rout->controller;
+$action = $rout->action;
 
 $controller->action($action);
 
