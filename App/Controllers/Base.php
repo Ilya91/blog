0698b50a77e99d1c5aeb555d544ade2417036bb7
Mironchikov Ilya <mir19on@mail.ru>
2016-05-10 13:57:43 +0300

    base controller

--- App/Controllers/Base.php
+++ App/Controllers/Base.php
@@ -12,4 +12,13 @@ namespace App\Controllers;
 abstract class Base extends Controller
 {
 
+    protected $title;		// заголовок страницы
+    protected $content;		// содержание страницы
+
+
+    public function render(){
+        $vars = array('content' => $this->content, 'title' => $this->title);
+        $page = $this->template(__DIR__ . '/../templates/main.php', $vars);
+        echo $page;
+    }
 }
