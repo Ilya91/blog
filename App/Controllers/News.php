5002c581a991a43586f60eb45e2d4ec104b2af6b
Mironchikov Ilya <mir19on@mail.ru>
2016-05-10 14:08:20 +0300

    beautiful News

--- App/Controllers/News.php
+++ App/Controllers/News.php
@@ -1,28 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 06.05.2016
- * Time: 21:51
- */
 
 namespace App\Controllers;
-use App\View;
 
-class News extends Controller
+class News extends Base
 {
-
-    protected $view;
-
-    /**
-     * News constructor.
-     */
-    public function __construct()
-    {
-        $this->view = new View();
-    }
-
-
     protected function beforeAction()
     {
     }
@@ -30,32 +11,18 @@ class News extends Controller
 
     protected function action_index()
     {
-        $this->view->display(__DIR__ . '/../templates/header.php');
-        $this->view->news = \App\Models\News::findLastNews();
-        $this->view->display(__DIR__ . '/../templates/index.php');
-        $this->view->display(__DIR__ . '/../templates/footer.php');
+        $this->title = 'Список новостей';
+        $news  = \App\Models\News::findLastNews();
+        $this->content = $this->template(__DIR__ . '/../templates/index.php', array('news' => $news));
     }
 
     protected function action_article()
     {
-        $url = $_SERVER['REQUEST_URI'];
-        $info = explode('/', $url);
-        $params = array();
-
-
-        foreach ($info as $v)
-        {
-            if ($v != '')
-                $params[] = $v;
+        $this->title = 'Статья';
+        if(isset($this->params[2])){
+            $id = $this->params[2];
+            $article = \App\Models\News::findById($id);
         }
-
-
-        $this->view->display(__DIR__ . '/../templates/header.php');
-        if(isset($params[2])){
-            $id = $params[2];
-            $this->view->news = \App\Models\News::findById($id);
-        }
-        $this->view->display(__DIR__ . '/../templates/article.php');
-        $this->view->display(__DIR__ . '/../templates/footer.php');
+        $this->content = $this->template(__DIR__ . '/../templates/article.php', array('article' => $article));
     }
 }
