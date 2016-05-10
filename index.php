007fc6bc125d657e05c2b3c3af522acd711bef70
Mironchikov Ilya <mir19on@mail.ru>
2016-05-10 10:13:43 +0300

    optimized frontcontroller

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
 require __DIR__ . '/autoload.php';
-
 $rout = new \App\Rout($_SERVER['REQUEST_URI']);
-$controller = $rout->controller;
-$action = $rout->action;
-
-$controller->action($action);
-
-
+$rout->Request();
